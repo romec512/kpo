76e7cdf4e1faeb84b5070208eaff0147678ff46b
Roman Asadov <roma-asadov@mail.ru>
2018-09-28 16:54:47 +0300

    part3, формирования списка данных

---
+++ lab1/Kpo4310_asadovrs.Lib/Source/Classes/MockSubstanceListCommand.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kpo4310_asadovrs.Lib
+{
+    public class MockSubstanceListCommand
+    {
+        public MockSubstanceListCommand()
+        {
+        }
+
+        private readonly string _dataFileName = "";
+        private List<Substance> _substanceList = null;
+        public List<Substance> substanceList
+        {
+            get { return _substanceList; }
+        }
+        public void Execute()
+        {
+            //Фигурная скобка для ограничения области видимости переменной substance
+            {
+                Substance substance = new Substance()
+                //данная констуркция позволяет инициализировать объект при его создании
+                {
+                    name = "Zn",
+                    type = 'M',
+                    lowTemperature = (float)0.8,
+                    highTemperature = (float)0.8
+
+                };
+                substanceList.Add(substance);
+            }
+            {
+                Substance substance = new Substance()
+                {
+                    name = "Pb-Au",
+                    type = 'П',
+                    lowTemperature = (float)2.0,
+                    highTemperature = (float)7.3
+
+                };
+                substanceList.Add(substance);
+            }
+            {
+                Substance substance = new Substance()
+                {
+                    name = "NbC",
+                    type = 'C',
+                    lowTemperature = (float)10.1,
+                    highTemperature = (float)10.5
+
+                };
+                substanceList.Add(substance);
+            }
+        }
+    }
+}
+
--- lab1/Kpo4310_asadovrs.Lib/Source/Classes/Substance.cs
+++ lab1/Kpo4310_asadovrs.Lib/Source/Classes/Substance.cs
@@ -3,8 +3,17 @@ namespace Kpo4310_asadovrs.Lib
 {
     public class Substance
     {
+        public string name {get; set;}
+        public char type {get; set; }
+        public float lowTemperature {get; set;}
+        public float highTemperature {get; set;}
+
         public Substance()
         {
+            name = "";
+            type = '';
+            lowTemperature = 0;
+            highTemperature = 0;
         }
     }
 }
