ce23e7c79dd62797aee24300c8a83c6c5ebd6884
Roman Asadov <roma-asadov@mail.ru>
2018-11-09 14:18:25 +0300

    добавил LogFileUtility

---
+++ lab1/Kpo4310_asadovrs.Lib/Source/Log/LogFileUtility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Kpo4310_asadovrs.Lib.Source.Log
+{
+    public class LogFileUtility
+    {
+        private string _fileName;
+        public string FileName {
+            get {
+                return _fileName;
+            }
+            set {
+                _fileName = value;
+            }
+        }
+
+        public LogFileUtility()
+        {
+            _fileName = "error.log";
+        }
+
+
+        public static void ErrorLog(string message){
+
+        }
+
+    }
+}
